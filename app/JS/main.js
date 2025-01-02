c586eb6aa4789f3e4fa437093ebe8386290bc485
Benjamin Yee <benyee1209@gmail.com>
2025-01-02 14:01:14 -0500

    asdasd
    asdasd

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,6 +1,5 @@
 import "../CSS/style.css"
 import { groceries } from "./groceries";
-//import {groceries} from "./products.js"
 
 const cardlocation = document.getElementById("cards");
 //default
@@ -154,23 +153,6 @@ document.querySelector(".Protein").addEventListener("click", function(){
 })
 })
 
-
-
-/*
-const dairy = groceries.filter(grocery => grocery.type.includes("Dairy"));
-console.log(dairy);
-
-const protein = groceries.filter(grocery => grocery.type.includes("Protein"));
-console.log(protein);
-
-const produce = groceries.filter(grocery => grocery.type.includes("Produce"));
-console.log(produce);
-
-const beverage = groceries.filter(grocery => grocery.type.includes("Beverage"));
-console.log(beverage);
-
-const grain = groceries.filter(grocery => grocery.type.includes("Grain"));
-console.log(grain);
-
-const pantry = groceries.filter(grocery => grocery.type.includes("Pantry"));
-console.log(pantry);*/
+/*function calculate{
+    
+}*/
